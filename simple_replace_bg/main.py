7af53be2fcb2ec20d9f6822e5a633b55eecab296
Chunlin TIAN <tianchunlin123@gmail.com>
2019-11-22 15:37:31 +0800

    Update main.py

--- simple_replace_bg/main.py
+++ simple_replace_bg/main.py
@@ -3,7 +3,27 @@ import numpy
 import os
 
 image = cv2.imread("../img/img.png", cv2.IMREAD_GRAYSCALE)
-_, thresh = cv2.threshold(image, 240, 255, cv2.THRESH_BINARY)
-cv2.bitwise_not(thresh, thresh)
-alpha = cv2.merge((thresh, thresh, thresh))
+_, mask = cv2.threshold(image, threshold, 255, cv2.THRESH_BINARY)
+kernel_white_hole = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+mask_open = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel_white_hole)
+
+cv2.bitwise_not(mask_open, mask_open)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
+mask_close = cv2.morphologyEx(mask_open, cv2.MORPH_CLOSE, kernel)
+
+kernel_1 = np.ones((5, 5), np.uint8)
+mask_erode = cv2.erode(mask_close, kernel_1)
+
+mask_erode = cv2.flip(mask_erode, 1, dst=None)
+mask_erode = cv2.erode(mask_erode, kernel_1)
+mask_erode = cv2.flip(mask_erode, 1, dst=None)
+
+mask_blur = cv2.GaussianBlur(mask_erode, (5, 5), 0)
+mask_blur = cv2.GaussianBlur(mask_blur, (5, 5), 0)
+
+
+mask_blur = cv2.medianBlur(mask_blur, 15)
+
+alpha = cv2.merge((mask_blur, mask_blur, mask_blur))
 cv2.imwrite("../img/alpha-simple.png", alpha)
